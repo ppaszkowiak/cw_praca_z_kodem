165af22a306889ab243328f89588998bc5f9384e
Patrycja Paszkowiak <patrycjapaszkowiak@air-patrycja.home>
2024-04-21 19:52:56 +0200

    zmiana

--- app.py
+++ app.py
@@ -11,4 +11,5 @@ def index():
 @app.route('/hello/')
 @app.route('/hello/<name>')
 def hello(name=None):
-    return render_template('hello.html', name=name)
+    my_name = 'Patrycja'
+    return render_template('hello.html', name='Patrycja')
